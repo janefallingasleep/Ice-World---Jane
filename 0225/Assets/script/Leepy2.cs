b6969a66a6b6849c43c9cfe777c9eef1e2ecd4a3
Jane <zzxia4869@gmail.com>
2018-02-26 13:11:27 -0800

    sound effect folder

---
+++ 0225/Assets/script/Leepy2.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Leepy2 : MonoBehaviour {
+	//public float speed = 10.0F;
+	//public float rotationSpeed = 100.0F;
+	void Update() {
+		//float translation = Input.GetAxis("Vertical") * speed;
+		//float rotation = Input.GetAxis("Horizontal") * rotationSpeed;
+		//translation *= Time.deltaTime;
+		//rotation *= Time.deltaTime;
+		transform.Translate(Input.GetAxis("Horizontal")*Time.deltaTime, 0f, 0f);
+		//transform.Rotate(0, rotation, 0);
+	}
+}
