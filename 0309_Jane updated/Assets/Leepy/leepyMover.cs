87fdb3c3d83f52f640026f9625c910a48f3abac1
EmmaYChen <yaochen.jo@gmail.com>
2018-03-13 23:34:02 -0700

    Emma - path, snowman

---
+++ 0309_Jane updated/Assets/Leepy/leepyMover.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+using System.Collections;
+using System.Collections.Generic;
+
+public class leepyMover : MonoBehaviour {
+
+	public List<Transform> points = new List <Transform> ();
+
+	private int destPoint = 0;
+	private NavMeshAgent agent;
+
+	public leepyWalkingPath path;
+	public float remainingDistance = 0.1f;
+	public int pointCount;
+
+	void Start () {
+
+		points = path.waypoints;
+		agent = GetComponent<NavMeshAgent>();
+
+		// Disabling auto-braking allows for continuous movement
+		// between points (ie, the agent doesn't slow down as it
+		// approaches a destination point).
+		agent.autoBraking = false;
+		GotoNextPoint();
+		pointCount = points.Count;
+	}
+
+	void GotoNextPoint() {
+		// Returns if no points have been set up
+		
+		if (pointCount == 0)
+			return;
+
+		// Set the agent to go to the currently selected destination.
+		agent.destination = points[destPoint].position;
+
+		// Choose the next point in the array as the destination,
+		// cycling to the start if necessary.
+		destPoint = (destPoint + 1) % points.Count;
+		pointCount = pointCount - 1;
+	}
+
+
+	void Update () {
+		// Choose the next destination point when the agent gets
+		// close to the current one.
+		if (agent.remainingDistance < remainingDistance)
+			GotoNextPoint();
+	}
+}
---
+++ 0309_Jane updated/Assets/Leepy/leepyWalkingPath.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class leepyWalkingPath : MonoBehaviour {
+
+	public List<Transform> waypoints = new List<Transform> ();
+	int index = 0;
+	public bool disableInGame;
+
+	void Update () {	
+		if (!Application.isPlaying) {
+			Transform[] tem = GetComponentsInChildren<Transform> ();
+			if (tem.Length > 0) {
+				waypoints.Clear ();
+				index = 0;
+				foreach (Transform t in tem) {
+					if (t != transform) {
+						t.name = "Way " + index.ToString ();
+						waypoints.Add (t);
+						index++;}
+					}
+			}
+		}
+	}
+
+	void OnDrawGizmos()
+	{
+		if (waypoints.Count > 0) {
+			Gizmos.color = Color.white;
+			foreach (Transform t in waypoints)
+				Gizmos.DrawSphere (t.position, 0.2f);
+			Gizmos.color = Color.black;
+			for (int a = 0; a < waypoints.Count - 1; a++)
+				Gizmos.DrawLine (waypoints [a].position, waypoints [a + 1].position);
+		}
+	}
+}
