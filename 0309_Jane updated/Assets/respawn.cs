33eef9330a5837ca72a8a53fa13eb53dd900f052
Aida Zhumabekova <adazoom@google.com>
2018-03-15 02:51:44 -0700

    Added respawn trigger on river

---
+++ 0309_Jane updated/Assets/respawn.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class respawn : MonoBehaviour 
+{
+	[SerializeField] public Transform player;
+	[SerializeField] public Transform camera;
+	[SerializeField] public Transform respawnPoint;
+
+	void OnTriggerEnter(Collider other)
+	{
+		Debug.Log ("Collides with " + other.gameObject.name);
+		if (other.gameObject.name == "shadow_rigged") {
+			player.transform.position = respawnPoint.transform.position;
+			camera.transform.position = new Vector3 (75f, 2.9f, 25f);
+		}
+	}
+}
